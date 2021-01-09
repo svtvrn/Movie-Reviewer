b81c024f1c42a2a8cce36bf45d07cd1ed9d2c923
svtvrn <nikoschasapis99@gmail.com>
2021-01-09 20:36:48 +0200

    upd

--- main.py
+++ main.py
@@ -1,22 +1,37 @@
-import numpy as np
 import matplotlib as plt
 from pathlib import Path
+
+def generateVectors(reviews,n,m):
+
+    for review in reviews:
+        reviewVector = []
+        score = int(review.split()[0])
+        for i in range(n,m):
+            if(" "+str(i)+":" in review):
+                reviewVector.append(1)
+            else:
+                reviewVector.append(0)
+        if(score>=7):
+            trainPosVectors.append(reviewVector)
+        elif(score<=4):
+            trainNegVectors.append(reviewVector)
 
 #The first "n" words in the vocabulary will be skipped
 n = 40
 #Every word after "m+n" won't be checked.
 m = 400
-trainDataVocab = open("aclImdb/train/labeledBow.feat","r")
-trainVectors = []
+numberOfNeg = 12500
+numberOfPos = 12500
 
+trainDataVocab = open("aclImdb/train/labeledBow.feat","r")
+trainPosVectors = []
+trainNegVectors = []
 reviews = trainDataVocab.readlines();
-for review in reviews:
-    score = review[0]
-    reviewVector = []
-    #print(score)
-    for i in range(n,m):
-        if(" "+str(i)+":" in review):
-            reviewVector.append(1)
-        else:
-            reviewVector.append(0)
-    trainVectors.append(reviewVector)
+generateVectors(reviews,n,m)
+
+print(len(trainNegVectors))
+print(len(trainPosVectors))
+
+#for vector in trainPosVectors:
+ #   for x in vector:
+  #      for i 
