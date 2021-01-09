16a1493a8741e3e51010f30d7942367d456d434d
svtvrn <nikoschasapis99@gmail.com>
2021-01-09 18:28:57 +0200

    update

--- main.py
+++ main.py
@@ -2,13 +2,21 @@ import numpy as np
 import matplotlib as plt
 from pathlib import Path
 
-#vocabFile = Path("aclImdb/imdb.vocab").absolute()
+#The first "n" words in the vocabulary will be skipped
+n = 40
+#Every word after "m+n" won't be checked.
+m = 400
+trainDataVocab = open("aclImdb/train/labeledBow.feat","r")
+trainVectors = []
 
-#The first "vocabStart" words in the vocabulary will be skipped
-vocabStart = 1000
-#Every word after "vocabEnd" won't be checked.
-vocabEnd = 20000
-
-testDataVocab = open("aclImdb/test/labeledBow.feat","r")
-reviewTokens = testDataVocab.readlines();
-print(reviewTokens[0])
+reviews = trainDataVocab.readlines();
+for review in reviews:
+    score = review[0]
+    reviewVector = []
+    #print(score)
+    for i in range(n,m):
+        if(" "+str(i)+":" in review):
+            reviewVector.append(1)
+        else:
+            reviewVector.append(0)
+    trainVectors.append(reviewVector)
