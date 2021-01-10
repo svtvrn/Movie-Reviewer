360f0a8c64ea589c02a217b26677886d9038f44a
svtvrn <nikoschasapis99@gmail.com>
2021-01-10 21:33:45 +0200

    id3 update

--- id3.py
+++ id3.py
@@ -38,23 +38,62 @@ def mostFrequentCategory(vectors):
     else:
         return 0
 
+def calculateConditionalProbability(vectors,x,value,C):
+    currentProbability = 0
+    scoreAppearances = 0
+    totalAppearances = 0
+    for vector in vectors:
+        score = vector[0]
+        if(vector[x]==value):
+            totalAppearances+=1
+            if(score==C):
+                scoreAppearances+=1
+    currentProbability = scoreAppearances/totalAppearances
+    return currentProbability*np.log2(currentProbability)
+        
+
+def calculateEntropy(vectors,x,value):
+    posReviewEntropy = calculateConditionalProbability(vectors,x,value,1)
+    negReviewEntropy = calculateConditionalProbability(vectors,x,value,0)
+    return (- posReviewEntropy - negReviewEntropy)
+
+def attributeProbabilty(vectors,x,value):
+    numberOfAppearances = 0
+    for vector in vectors:
+        if(vector[x]==value):
+            numberOfAppearances+=1
+
+    return numberOfAppearances/len(vectors)
+
+def bestAttributeSelection(vectors,m):
+    maxAttributeGain = 0
+    maxInfoGain  = 0
+    for x in range(1,m+1):
+        infoGain = entropy - attributeProbabilty(vectors,x,1) * calculateEntropy(vectors,x,1) -  attributeProbabilty(vectors,x,0) *  calculateEntropy(vectors,x,0)
+        if(infoGain>maxInfoGain):
+            maxInfoGain = infoGain
+            maxAttributeGain = x
+    return maxAttributeGain
+
 def trainDataId3(vectors,n,m,defaultClf):
 
     if(vectors==None):
         return defaultClf
     elif(categoriseVectors(vectors)):
+        print("CATEGORISE VECTORS")
         return vectors[0][0]
     elif(m<=0):
         return mostFrequentCategory(vectors)
     else:
+        print("FIND BEST")
         bestAttribute = bestAttributeSelection(vectors,m)
-
+        print(bestAttribute)
 
 
 #The first "n" words in the vocabulary will be skipped
 n = 45
 #Every word after "m+n" won't be checked.
-m = 200
+m = 50
 #Class probability
 C=0.5
 #Starting entropy
@@ -68,4 +107,5 @@ generateVectors(reviews,n,m)
 
 #True equals a good review, False equals a bad review
 defaultClf = True
+print("TRAIN START")
 id3Tree = trainDataId3(trainVectors,n,m,defaultClf)
