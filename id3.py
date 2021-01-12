6d85865eca18683b08f42ed427fd35194bfe2f65
svtvrn <nikoschasapis99@gmail.com>
2021-01-12 13:43:45 +0200

    update

--- id3.py
+++ id3.py
@@ -23,113 +23,118 @@ def categoriseVectors(vectors):
     for i in range(1,len(vectors)):
         if(category!=vectors[i].get('clf')):
             return False
-    return True
+        if(i==(0.9)*len(vectors)):
+            return True
 
 def mostFrequentCategory(vectors):
 
-    posCounter = 0
-    negCounter = 0
+    pos_counter = 0
+    neg_counter = 0
     for vector in vectors:
         if(vector.get('clf')==1):
-            posCounter+=1
+            pos_counter+=1
         elif(vector.get('clf')==0):
-            negCounter+=1
-    if(posCounter>=negCounter):
+            neg_counter+=1
+    if(pos_counter>=neg_counter):
         return True
     else:
         return False
 
 def calculateCondProbability(vectors,x,value,C):
-    currentProbability = 0
-    scoreAppearances = 0
-    totalAppearances = 0
+    current_probability = 0
+    score_appearances = 0
+    total_appearances = 0
     for vector in vectors:
         score = vector.get('clf')
         if(vector.get(x)==value):
-            totalAppearances+=1
+            total_appearances+=1
             if(score==C):
-                scoreAppearances+=1
-    if(totalAppearances>0):
-        currentProbability = scoreAppearances/totalAppearances
+                score_appearances+=1
+    if(total_appearances>0):
+        current_probability = score_appearances/total_appearances
     else:
-        currentProbability = 0.001
-    if(currentProbability<=0):
-        currentProbability = 0.001
-    return currentProbability*np.log2(currentProbability)
+        current_probability = 0.001
+    if(current_probability<=0):
+        current_probability = 0.001
+    return current_probability*np.log2(current_probability)
         
 def calculateCondEntropy(vectors,x,value):
-    posReviewEntropy = calculateCondProbability(vectors,x,value,1)
-    negReviewEntropy = calculateCondProbability(vectors,x,value,0)
-    return ( - posReviewEntropy - negReviewEntropy)
+    pos_review_entropy = calculateCondProbability(vectors,x,value,1)
+    neg_review_entropy = calculateCondProbability(vectors,x,value,0)
+    return ( - pos_review_entropy - neg_review_entropy)
 
 def calculateInfoGain(vectors,x):
     #Number of attribute showing up in reviews
-    attrAppearances = 0
+    attr_appearances = 0
     #Number of positive and negative reviews
-    posReviews = 0
-    negReviews = 0
+    pos_reviews = 0
+    neg_reviews = 0
     #For each review
     for vector in vectors:
         if(vector.get(x)==1):
-            attrAppearances+=1
+            attr_appearances+=1
         if(vector.get('clf')==1):
-            posReviews+=1
+            pos_reviews+=1
         else:
-            negReviews += 1
+            neg_reviews += 1
     #Percentage of pos and neg reviews to calculate entropy
-    posReviewPerc = posReviews/len(vectors)
-    negReviewPerc = negReviews/len(vectors)
+    pos_review_perc = pos_reviews/len(vectors)
+    neg_review_perc = neg_reviews/len(vectors)
     #Calculating entropy
-    entropy = -posReviewPerc*np.log2(posReviewPerc) - negReviewPerc*np.log2(negReviewPerc)
+    if(pos_review_perc==0):
+        pos_review_perc=0.001
+    elif(neg_review_perc==0):
+        neg_review_perc=0.001
+    entropy = -pos_review_perc*np.log2(pos_review_perc) - neg_review_perc*np.log2(neg_review_perc)
     #Probability of finding and not finding the attribute in a review
-    attrProbability = attrAppearances/len(vectors)
-    return (entropy - attrProbability*calculateCondEntropy(vectors,x,1) - (1-attrProbability)*calculateCondEntropy(vectors,x,0))
+    attr_probability = attr_appearances/len(vectors)
+    return (entropy - attr_probability*calculateCondEntropy(vectors,x,1) - (1-attr_probability)*calculateCondEntropy(vectors,x,0))
 
 def bestAttributeSelection(vectors):
 
     attributes = list(vectors[0].keys())
     attributes.remove("clf")
-    maxAttribute = 0
-    maxInfoGain  = 0
+    max_attribute = 0
+    max_info_gain  = 0
     for x in attributes:
-        infoGain = calculateInfoGain(vectors,x)
-        if(infoGain>maxInfoGain):
-            maxInfoGain = infoGain
-            maxAttribute = x
-    return maxAttribute
+        info_gain = calculateInfoGain(vectors,x)
+        if(info_gain>max_info_gain):
+            max_info_gain = info_gain
+            max_attribute = x
+    return max_attribute
 
-def trainDataId3(vectors,freqCategory):
+def trainDataId3(vectors,freq_category):
     tree=[]
     if(len(vectors)==0):
-        return freqCategory
+        return freq_category
     if(categoriseVectors(vectors)):
         return  vectors[0].get('clf')
     if(len(vectors[0])<=1):
         return mostFrequentCategory(vectors)
-    highestIGAttr = bestAttributeSelection(vectors) 
+    best_attr = bestAttributeSelection(vectors) 
     #print('Vocab @:',highestIGAttr)
-    tree.append(highestIGAttr)
-    leftVectors = []
-    rightVectors = []
+    tree.append(best_attr)
+    left_vectors = []
+    right_vectors = []
     category = mostFrequentCategory(vectors)
-    if(highestIGAttr>0):
+    if(best_attr>0):
         for vector in vectors:
-            attrValue = vector.pop(highestIGAttr)
-            if(attrValue == 1):
-                leftVectors.append(vector)
+            attr_value = vector.pop(best_attr)
+            if(attr_value == 1):
+                left_vectors.append(vector)
             else:
-                rightVectors.append(vector)
+                right_vectors.append(vector)
             vectors.remove(vector)
-    leftTree = trainDataId3(leftVectors,category)
-    tree.append(leftTree)
-    rightTree = trainDataId3(rightVectors,category)
-    tree.append(rightTree)
+    left_tree = trainDataId3(left_vectors,category)
+    tree.append(left_tree)
+    right_tree = trainDataId3(right_vectors,category)
+    tree.append(right_tree)
     return tree
         
 #The first "n-1" words in the vocabulary will be skipped
-n = 1000
+n = 70
 #Every word after "m+n" won't be checked.
-m = 10
+m = 20
 entropy = -0.5*np.log2(0.5) - 0.5*np.log2(0.5)
 
 trainData = open("aclImdb/train/labeledBow.feat","r")
@@ -142,26 +147,22 @@ tests = generateVectors(testData.readlines(),n,m)
 
 print(id3tree)
 
-accuracy = 0
+accuracy = 200
 for test in tests:
-
     node = id3tree
     nodeKey = test.get(node[0])
-    value = 0
     score = test.get('clf')
-    depth = 1
     while(True):
-        if(value==1):
+        if(nodeKey==1):
             node = node[1]
             nodeKey = test.get(node[0])
-        elif(value==0):
+        elif(nodeKey==0):
             node = node[2]
             nodeKey = test.get(node[0])
         if(nodeKey==True or nodeKey==False):
-            value = nodeKey
+            if(nodeKey==score):
+                accuracy+=1
             break
-        depth+=1
-    if(value==score):
-        accuracy+=1
+    
 
 print(accuracy/250,"%")
