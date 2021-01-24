3bb8d5e946bf0fcf1e9af0efd7e2bd45dc8fc6f4
svtvrn <nikoschasapis99@gmail.com>
2021-01-24 03:55:25 +0200

    updates

--- adaboost.py
+++ adaboost.py
@@ -1,70 +1,76 @@
 import numpy as np
 import random as rand
 
+
 class Stump:
 
-    #Decision stump constructor, featuring a root
-    #with the attribute from the samples.
-    def __init__(self,root,left,right):
+    # Decision stump constructor, featuring a root
+    # with the attribute from the samples.
+    def __init__(self, root, left, right):
         self.root = root
-        self.left = left 
+        self.left = left
         self.right = right
-    
-    #Checks if the stump predicted correctly.
-    def check_sample(self,sample):
+
+    # Checks if the stump predicted correctly.
+    def check_sample(self, sample):
         decision = sample[0][self.root]
         sample_clf = sample[1]
-        if(decision==1):
-            return self.left==sample_clf
+        if(decision == 1):
+            return self.left == sample_clf
         else:
-            return self.right==sample_clf
+            return self.right == sample_clf
 
-    #Stump print
+    # Stump print
     def print_stump(self):
-        print('Root: ',self.root,' Left: ',self.left, ' Right: ',self.right)
+        print('Root: ', self.root, ' Left: ',
+              self.left, ' Right: ', self.right)
+
 
 class AdaboostClf:
 
-    def __init__(self,h,z):
+    def __init__(self, h, z):
         self.h = h
         self.z = z
 
-    def test(self,sample):
-        is_positive=0
-        is_negative=0
+    def test(self, sample):
+        is_positive = 0
+        is_negative = 0
         threshold = 0.8
         for i in range(len(self.h)):
             answer = sample[0][self.h[i].root]
-            if(answer==1):
+            if(answer == 1):
                 if(self.h[i].left):
-                    is_positive+= self.z[i]
+                    is_positive += self.z[i]
                 else:
-                    is_negative+= self.z[i]
-            else:   
+                    is_negative += self.z[i]
+            else:
                 if(self.h[i].right):
-                    is_positive+= self.z[i]
+                    is_positive += self.z[i]
                 else:
-                    is_negative+= self.z[i]
+                    is_negative += self.z[i]
 
-            if is_positive==threshold*len(self.h) or is_negative==threshold*len(self.h):
-                return is_positive>is_negative 
+            if is_positive == threshold*len(self.h) or is_negative == threshold*len(self.h):
+                return is_positive > is_negative
 
-        return is_positive>is_negative
+        return is_positive > is_negative
 
-#Generating sample data in tuples
-def generate_samples(samples,n,m):
+# Generating sample data in tuples
+
+
+def generate_samples(samples, n, m):
     data = []
     for sample in samples:
         sample_data = []
-        score = int(sample.split()[0])>5
-        for i in range(n,m+n):
+        score = int(sample.split()[0]) > 5
+        for i in range(n, m+n):
             if(" "+str(i)+":" in sample):
                 sample_data.append(1)
             else:
                 sample_data.append(0)
-        tuple = (sample_data,score)
+        tuple = (sample_data, score)
         data.append(tuple)
     return data
+
 
 def normalize(weights):
     weight_sum = 0
@@ -74,132 +80,148 @@ def normalize(weights):
         weights[i] /= weight_sum
     return weights
 
-def calculate_gini(samples,attr):
 
-    true_pos = 0; false_pos = 0
-    true_neg = 0; false_neg = 0
+def calculate_gini(samples, attr):
+
+    true_pos = 0
+    false_pos = 0
+    true_neg = 0
+    false_neg = 0
     for sample in samples:
-        if(sample[0][attr]==1):
+        if(sample[0][attr] == 1):
             if(sample[1]):
-                true_pos+=1
+                true_pos += 1
             else:
-                false_pos+=1
+                false_pos += 1
         else:
             if(not sample[1]):
-                true_neg+=1
+                true_neg += 1
             else:
-                false_neg+=1
-    pos_prob = 1 - pow((true_pos+1)/(true_pos+false_pos+2),2) - pow((false_pos+1)/(true_pos+false_pos+2),2)
-    neg_prob = 1 - pow((true_neg+1)/(true_neg+false_neg+2),2) - pow((false_neg+1)/(true_neg+false_neg+2),2)
-    impurity = (true_pos+false_pos)/(true_pos+false_pos+true_neg+false_neg)*pos_prob + (true_neg+false_neg)/(true_pos+false_pos+true_neg+false_neg)*neg_prob
-    return impurity, true_pos>false_pos
+                false_neg += 1
+    pos_prob = 1 - pow((true_pos+1)/(true_pos+false_pos+2), 2) - \
+        pow((false_pos+1)/(true_pos+false_pos+2), 2)
+    neg_prob = 1 - pow((true_neg+1)/(true_neg+false_neg+2), 2) - \
+        pow((false_neg+1)/(true_neg+false_neg+2), 2)
+    impurity = (true_pos+false_pos)/(true_pos+false_pos+true_neg+false_neg) * \
+        pos_prob + (true_neg+false_neg) / \
+        (true_pos+false_pos+true_neg+false_neg)*neg_prob
+    return impurity, true_pos > false_pos
+
 
 def weak_learner(samples):
 
     min_gini = np.inf
-    best_attr = 0 
+    best_attr = 0
     best_has_attr = True
-    #Calculating gini impurity for each  
-    #available attribute and chooosing  
-    #the one with the lowest one
+    # Calculating gini impurity for each
+    # available attribute and chooosing
+    # the one with the lowest one
     for attr in range(len(samples[0][0])):
-        gini, has_attr = calculate_gini(samples,attr)
-        if(gini<min_gini):
+        gini, has_attr = calculate_gini(samples, attr)
+        if(gini < min_gini):
             min_gini = gini
             best_attr = attr
             best_has_attr = has_attr
-    #We create the decision stump based  
-    #on the best attribute selected
+    # We create the decision stump based
+    # on the best attribute selected
     return Stump(best_attr, best_has_attr, not best_has_attr)
 
-def repopulate_samples(samples,weights):
-    #Preparing the weight buckets for the sample distribution.
-    weight_buckets = [weights[0]]
-    for i in range(1,len(weights)):
-        weight_buckets.append( weights[i] + weight_buckets[i-1] )
-    #Filling the sample data again.
-    new_samples = rand.choices(samples, cum_weights = weight_buckets, k= len(samples))
-    #Balancing the sample weights.
-    weights = [1/len(samples)]*len(samples)
-    return new_samples,weights
-    
-def weighted_majority(h,z):
-    return AdaboostClf(h,z)
 
-def adaboost(samples,iterations):
-    #Number of total weights
+def repopulate_samples(samples, weights):
+    # Preparing the weight buckets for the sample distribution.
+    weight_buckets = [weights[0]]
+    for i in range(1, len(weights)):
+        weight_buckets.append(weights[i] + weight_buckets[i-1])
+    # Filling the sample data again.
+    new_samples = rand.choices(
+        samples, cum_weights=weight_buckets, k=len(samples))
+    # Balancing the sample weights.
+    weights = [1/len(samples)]*len(samples)
+    return new_samples, weights
+
+
+def weighted_majority(h, z):
+    return AdaboostClf(h, z)
+
+
+def adaboost(samples, iterations):
+    # Number of total weights
     w = [1/len(samples)]*len(samples)
-    #Number of hypotheses we've learned.
+    # Number of hypotheses we've learned.
     h = []
-    #The ammount of say for each hypothesis.
+    # The ammount of say for each hypothesis.
     z = []
-    for i in range (iterations):
-        #After the first loop we have to keep repopulating the samples according to their weights.
-        if i!=0:
-            samples,w = repopulate_samples(samples,w)
+    for i in range(iterations):
+        # After the first loop we have to keep repopulating the samples according to their weights.
+        if i != 0:
+            samples, w = repopulate_samples(samples, w)
         stump = weak_learner(samples)
         h.append(stump)
         error = 0
-        #Calculating total error from every sample.
-        for j in range (len(samples)):
+        # Calculating total error from every sample.
+        for j in range(len(samples)):
             if not h[i].check_sample(samples[j]):
                 error += w[j]
-        #We add the stump weight (amount of say) to the list, if the error is 0.5 then we don't take
-        #the stump into account.
-        if error == 0.5 :
+        # We add the stump weight (amount of say) to the list, if the error is 0.5 then we don't take
+        # the stump into account.
+        if error == 0.5:
             z.append(0)
         else:
             z.append(0.5*np.log(((1-error)/(error+0.0001))+1))
-        #Updating the sample weights based on the erros we made previously.
-        for j in range (len(samples)):
+        # Updating the sample weights based on the erros we made previously.
+        for j in range(len(samples)):
             if h[i].check_sample(samples[j]):
                 w[j] *= np.exp(-z[i])
             else:
                 w[j] *= np.exp(z[i])
-        #Normalizing the sample weights so they add up to 1.
-        w = normalize(w)        
-    return weighted_majority(h,z)
+        # Normalizing the sample weights so they add up to 1.
+        w = normalize(w)
+    return weighted_majority(h, z)
 
-def run_tests(adaboost,tests):
+
+def run_tests(adaboost, tests):
     accuracy = 0
-    true_pos=0; true_neg=0
-    false_pos = 0; false_neg = 0
+    true_pos = 0
+    true_neg = 0
+    false_pos = 0
+    false_neg = 0
     for test in tests:
         clf = adaboost.test(test)
         if(clf == test[1]):
-            accuracy+=1
-            if(clf==True):
-                true_pos+=1
+            accuracy += 1
+            if(clf == True):
+                true_pos += 1
             else:
-                true_neg+=1
+                true_neg += 1
         else:
             if(clf):
-                false_pos+=1
+                false_pos += 1
             else:
-                false_neg+=1
+                false_neg += 1
     precision = true_pos/(true_pos + false_pos)
     recall = true_pos/(true_pos + false_neg)
     f1 = 2*(recall*precision)/(recall+precision)
     print('Precision: ', precision,)
     print('Recall:', recall)
     print('F1: ', f1)
-    print('Accuracy: ', accuracy/len(tests)*100,"%")
+    print('Accuracy: ', accuracy/len(tests)*100, "%")
 
-#The first "n-1" words in the vocabulary will be skipped
+
+# The first "n-1" words in the vocabulary will be skipped
 n = 75
-#Every word after "m+n" won't be checked.
+# Every word after "m+n" won't be checked.
 m = 400
-#Number of iterations Adaboost will perfrom
+# Number of iterations Adaboost will perfrom
 iterations = 30
 
-#Loading the training data
-train_data = open("aclImdb/train/labeledBow.feat","r")
-train_samples = generate_samples(train_data.readlines(),n,m)
+# Loading the training data
+train_data = open("aclImdb/train/labeledBow.feat", "r")
+train_samples = generate_samples(train_data.readlines(), n, m)
 train_samples = train_samples[0:4000] + train_samples[12500:16500]
 
-#Loading the test data
-adaboost_clf = adaboost(train_samples,iterations)
-test_data = open("aclImdb/test/labeledBow.feat","r")
-test_samples = generate_samples(test_data.readlines(),n,m)
-test_samples  = test_samples[0:1500] + test_samples [12500:14000]
-run_tests(adaboost_clf,test_samples)
+# Loading the test data
+adaboost_clf = adaboost(train_samples, iterations)
+test_data = open("aclImdb/test/labeledBow.feat", "r")
+test_samples = generate_samples(test_data.readlines(), n, m)
+test_samples = test_samples[0:1500] + test_samples[12500:14000]
+run_tests(adaboost_clf, test_samples)
--- naive_bayes.py
+++ naive_bayes.py
@@ -1,108 +1,116 @@
 import random as rand
 
-def generateVectors(reviews,n,m):
+
+def generateVectors(reviews, n, m):
 
     for review in reviews:
         reviewVector = []
         score = int(review.split()[0])
-        for i in range(n,m+n):
+        for i in range(n, m+n):
             if(" "+str(i)+":" in review):
                 reviewVector.append(1)
             else:
                 reviewVector.append(0)
-        if(score>=7):
+        if(score >= 7):
             trainPosVectors.append(reviewVector)
-        elif(score<=4):
+        elif(score <= 4):
             trainNegVectors.append(reviewVector)
 
-def trainData(trainVector,wordProbability,n,m):
+
+def trainData(trainVector, wordProbability, n, m):
 
     for x in range(m):
         currProbability = 0
         numberOfAppearances = 0
         for vector in trainVector:
-            if(vector[x]==1):
-                numberOfAppearances +=1
+            if(vector[x] == 1):
+                numberOfAppearances += 1
         currProbability = (numberOfAppearances+1)/(len(trainVector)+2)
         wordProbability.append(currProbability)
 
-def naiveBayes(testVector,wordProbability):
 
-    probability=1
+def naiveBayes(testVector, wordProbability):
+
+    probability = 1
     for x in range(m):
-        if(testVector[x]==1):
-            probability*= wordProbability[x]
-        elif(testVector[x]==0):
-            probability*= (1 - wordProbability[x])
+        if(testVector[x] == 1):
+            probability *= wordProbability[x]
+        elif(testVector[x] == 0):
+            probability *= (1 - wordProbability[x])
     probability *= 0.5
     return probability
 
-def checkAccuracy(posReview,negReview,score):
 
-    if(posReview>negReview and score>6):
+def checkAccuracy(posReview, negReview, score):
+
+    if(posReview > negReview and score > 6):
         return True
-    elif(posReview<negReview and score<5):
+    elif(posReview < negReview and score < 5):
         return True
     return False
+
 
 def run_tests(tests):
     threshold = 1
     accuracy = 0
-    true_pos=0; true_neg=0
-    false_pos = 0; false_neg = 0
+    true_pos = 0
+    true_neg = 0
+    false_pos = 0
+    false_neg = 0
 
     for test in tests:
         testVector = []
         score = int(test.split()[0])
-        for i in range(n,m+n):
+        for i in range(n, m+n):
             if(" "+str(i)+":" in test):
                 testVector.append(1)
             else:
                 testVector.append(0)
-        posReviewProbability = naiveBayes(testVector,posWordProbabilities)
-        negReviewProbability = naiveBayes(testVector,negWordProbabilities)
+        posReviewProbability = naiveBayes(testVector, posWordProbabilities)
+        negReviewProbability = naiveBayes(testVector, negWordProbabilities)
         negReviewProbability *= threshold
-        if(checkAccuracy(posReviewProbability,negReviewProbability,score)):
-            accuracy+=1
-            if(posReviewProbability>negReviewProbability):
-                true_pos+=1
+        if(checkAccuracy(posReviewProbability, negReviewProbability, score)):
+            accuracy += 1
+            if(posReviewProbability > negReviewProbability):
+                true_pos += 1
             else:
-                true_neg+=1
+                true_neg += 1
         else:
-            if(posReviewProbability>negReviewProbability):
-                false_pos+=1
+            if(posReviewProbability > negReviewProbability):
+                false_pos += 1
             else:
-                false_neg+=1
+                false_neg += 1
     precision = true_pos/(true_pos + false_pos)
     recall = true_pos/(true_pos + false_neg)
     f1 = 2*(recall*precision)/(recall+precision)
     print('Precision: ', precision,)
     print('Recall:', recall)
     print('F1: ', f1)
-    print('Accuracy: ', accuracy/len(tests)*100,"%")
+    print('Accuracy: ', accuracy/len(tests)*100, "%")
 
-#The first "n" words in the vocabulary will be skipped
+
+# The first "n" words in the vocabulary will be skipped
 n = 75
-#Every word after "m+n" won't be checked.
+# Every word after "m+n" won't be checked.
 m = 1000
-#Loads the training token file and splits the reviews into lines
-trainDataVocab = open("aclImdb/train/labeledBow.feat","r")
+# Loads the training token file and splits the reviews into lines
+trainDataVocab = open("aclImdb/train/labeledBow.feat", "r")
 reviews = trainDataVocab.readlines()
 
-#Positive and negative review vectors
+# Positive and negative review vectors
 trainPosVectors = []
 trainNegVectors = []
 
 posWordProbabilities = []
 negWordProbabilities = []
 
-#Filling up the two vectors with the review tokens
-generateVectors(reviews,n,m)
-trainData(trainPosVectors,posWordProbabilities,n,m)
-trainData(trainNegVectors,negWordProbabilities,n,m)
+# Filling up the two vectors with the review tokens
+generateVectors(reviews, n, m)
+trainData(trainPosVectors, posWordProbabilities, n, m)
+trainData(trainNegVectors, negWordProbabilities, n, m)
 
-#TESTING
-testDataVocab = open("aclImdb/test/labeledBow.feat","r")
+# TESTING
+testDataVocab = open("aclImdb/test/labeledBow.feat", "r")
 testReviews = testDataVocab.readlines()
-testReviews = testReviews[0:1500] + testReviews [12500:14000]
+testReviews = testReviews[0:1500] + testReviews[12500:14000]
 run_tests(testReviews)
